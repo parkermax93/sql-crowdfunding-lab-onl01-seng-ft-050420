43ce9a6aacbb38711368d9f305b4b6fe8436a23a
Parker Williamson <parker.williamson@gmail.com>
2020-06-02 00:43:02 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,3 @@
-CREATE TABLE projects (title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
 
   
